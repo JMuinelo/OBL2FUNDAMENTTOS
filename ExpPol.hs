70fa407c63b1f3ccb36d631a2b919b9581842ca2
Santiago Alvariza <santiagoalvarizapan@gmail.com>
2025-11-25 13:17:17 -0300

    faltan 13 14 15
    aaaaaa

--- ExpPol.hs
+++ ExpPol.hs
@@ -1,9 +1,12 @@
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 {-#LANGUAGE GADTs#-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use lambda-case" #-}
+{-# HLINT ignore "Redundant lambda" #-}
 
 module ExpPol where
 
-import Polinomios
+import Polinomios;
 
 -- ========================
 -- EXPRESIONES POLINOMICAS
@@ -20,15 +23,80 @@ data ExPol where
 																	
 --10) 
 cantPol :: ExPol -> Int
-cantPol = undefined 
+cantPol = \x -> case x of {
+	
+	 	Pol f -> 1;
+	
+		Der a -> cantPol a;
+	
+		Eval a n -> cantPol a;
+	
+		Sum xs -> sum (map cantPol xs) ;
+	
+		Prod xs -> sum (map cantPol xs)
+	
+	}
+
+
+
 
 --11)
-cantx :: ExPol -> Int
-cantx  = undefined
+cantx:: ExPol -> Int
+cantx = \x -> case x of {
+	
+	 	Pol f -> case f of{
+			[] -> 0;
+			(c,g) : fs -> case g of {
+				0 -> cantx (Pol fs);
+				x -> 1 + cantx (Pol fs);
+			} 
+		} ;
+	
+		Der a -> cantx a;
+	
+		Eval a n -> cantx a;
+	
+		Sum xs -> case xs of{
+			[] -> 0;
+			y:ys -> cantx y +  cantx(Sum ys);
+		} ;
+	
+		Prod xs -> case xs of{
+			[] -> 0;
+			y:ys -> cantx y +  cantx(Prod ys);
+		} 
+	
+		
+
+	
+	}
+
 
 --12)
 maxProd :: ExPol -> Int
-maxProd  = undefined
+maxProd  = \x -> case x of {
+	
+	 	Pol f -> 1;
+	
+		Der a -> maxProd a;
+	
+		Eval a n -> maxProd a;
+	
+		Sum xs -> case xs of{
+			[] -> 0;--"sera cero o no nunca lo sabremos"mud wizard 2025
+			y:ys -> max (maxProd y) (maxProd (Sum ys));
+		};
+	
+		Prod xs -> case xs of{
+			[] -> 0;
+			y:ys -> max (maxProd y) (maxProd (Prod ys));
+			
+		} 
+		
+		
+
+	
+		}
 
 
 --13)
